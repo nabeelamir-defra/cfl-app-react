b917af57bcac190083f29db00017475b5038be1a
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2022-04-21 14:49:07 +0100

    added useInput hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Main, InputField, Button, FormGroup, Heading } from 'govuk-react'
 
+import { useInput } from './hooks/useInput'
+
 function App() {
+  const surname = useInput("")
+
+  const submitForm = (event) => {
+    event.preventDefault()
+    console.log("surname", surname.value)
+  }
   return (
     <Main>
       <Heading size="LARGE">Check a Fishing Licence</Heading>
-      <FormGroup>
-        <InputField
-          input={{
-            name: 'group0'
-          }}
-        >
-          Surname
-        </InputField>
-      </FormGroup>
-      <Button>Find</Button>
+      <form onSubmit={submitForm}>
+        <FormGroup>
+          <InputField {...surname}>Surname</InputField>
+        </FormGroup>
+        <Button>Find</Button>
+      </form>
     </Main>
   );
 }
